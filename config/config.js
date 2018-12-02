5f5de2c0dbd840b53c1b3f8f0ae3bd222e177881
iremit <mis@iremits-MacBook-Pro.local>
2018-12-02 20:47:34 +0800

    make it better

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/..');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'quotes-journal'
+    },
+    port: process.env.PORT || 3000,
+    db: process.env.CLEARDB_DATABASE_URL || 'mysql://root:alwindeleon@localhost/quotes_journal'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'quotes-journal'
+    },
+    port: process.env.PORT || 3000,
+    db: process.env.CLEARDB_DATABASE_URL || 'mysql://localhost/quotes_journal'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'quotes-journal'
+    },
+    port: process.env.PORT || 3000,
+    db: 'process.env.CLEARDB_DATABASE_URL || mysql://localhost/quotes_journal'
+  }
+};
+
+module.exports = config[env];
