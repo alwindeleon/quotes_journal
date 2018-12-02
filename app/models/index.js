0162572cb7edd193e29dafe56d68460ad5153dc1
iremit <mis@iremits-MacBook-Pro.local>
2018-12-02 20:58:58 +0800

    make it better

--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const config = require('../../config/config');
 const db = {};
 
+console.log(config.db);
 const sequelize = new Sequelize(config.db);
 
 fs.readdirSync(__dirname)
